92eeea365028c804ea793ede888fc33a6ad921f5
Thibaut Jombart <thibautjombart@gmail.com>
2022-04-12 11:42:12 +0200

    adding empty function for constructor

---
+++ R/make_linelist.R
@@ -0,0 +1,28 @@
+#' Title of the function
+#'
+#' A short description of what it does, the main inputs and outputs.
+#'
+#' @param x a `data.frame` or a `tibble` containing case line list data, with
+#'   cases in rows and variables in columns
+#'
+#' @export
+#'
+#' @author Thibaut Jombart [thibaut@data.org](thibaut@data.org)
+#'
+#' @return The function returns a `linelist` object.
+#'
+#' @details Some more information on the function
+#'
+#' @examples
+#' # basic use of the function
+#' template(1)
+#' 
+
+make_linelist <- function(x) {
+  # assert inputs
+
+  # do stuff ...
+
+  # shape output and return object
+  x
+}
