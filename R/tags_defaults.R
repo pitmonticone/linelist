f20f655180d315e0136c627976dc813004fc265d
Thibaut Jombart <thibautjombart@gmail.com>
2022-04-13 10:50:35 +0200

    adding 2 small auxil functions

---
+++ R/tags_defaults.R
@@ -0,0 +1,30 @@
+#' Generate default tags for a linelist
+#'
+#' This function returns a named list providing the default tags for a
+#' `linelist` object (all default to NULL).
+#'
+#' @export
+#'
+#' @author Thibaut Jombart [thibaut@data.org](thibaut@data.org)
+#'
+#' @return A named `list`.
+#'
+#' @examples
+#' tags_defaults()
+
+tags_defaults <- function() {
+  list(
+    date_onset = NULL,
+    date_reporting = NULL,
+    date_admission = NULL,
+    date_discharge = NULL,
+    date_outcome = NULL,
+    date_death = NULL,
+    gender = NULL,
+    age = NULL,
+    location = NULL,
+    occupation = NULL,
+    hcw = NULL, 
+    outcome = NULL
+  )
+}
---
+++ R/tags_names.R
@@ -0,0 +1,20 @@
+#' Get the list of tag names used in linelist
+#'
+#' This function returns the a `character` of all tag names used to designate
+#' specific variable types in a `linelist`.
+#'
+#' @export
+#'
+#' @author Thibaut Jombart [thibaut@data.org](thibaut@data.org)
+#'
+#' @return The function returns a `character` vector.
+#'
+#' @seealso [tags_defaults](tags_defaults) for a `list` of default values of the
+#'   tags
+#' 
+#' @examples
+#' tags_names()
+
+tags_names <- function() {
+  names(tags_defaults())
+}
