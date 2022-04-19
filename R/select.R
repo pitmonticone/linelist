e8bdd009c77f12a31d11d9d1ef407c7a134d267b
Thibaut Jombart <thibautjombart@gmail.com>
2022-04-19 10:17:40 +0200

    adding tag specific accessors

--- R/select.R
+++ R/select.R
@@ -3,8 +3,12 @@
 #' This function works similarly to `dplyr::select` but can in addition refer to
 #' tagged variables. We recommend referring to variables and tags by their
 #' names: using `logical` and `integer` may have unexecpted results due to how
-#' regular variables and tagged variables are combined (see details).
+#' regular variables and tagged variables are combined (see details). Functions
+#' in the form `select_[tag]` are wrappers for accessing specific tags as listed
+#' in `tags_names()`.
 #'
+#' @rdname select.linelist
+#' 
 #' @param .data a `linelist` object
 #'
 #' @param ... the variables to select, either using their column names, or tag
@@ -27,6 +31,9 @@
 #'   approach. Other approaches using `integer` or `logical` may yield more
 #'   confusing outputs, e.g. selecting `all` variables using `TRUE` will return
 #'   the two data.frames concatenated.
+#'
+#' @seealso [`make_linelist`](make_linelist) for a list of all tags which can be
+#'   used in `select`
 #' 
 
 select.linelist <- function(.data, ..., warn = TRUE) {
@@ -47,4 +54,85 @@ select.linelist <- function(.data, ..., warn = TRUE) {
   class(out) <- c("linelist", class(out))
   out <- prune_tags(out, warn = warn)
   out
+}
+
+
+
+#' @export
+#' @rdname select.linelist
+#' @param x a `linelist` object
+select_id <- function(x) {
+  select(x, "id")
+}
+
+#' @export
+#' @rdname select.linelist
+select_date_onset <- function(x) {
+  select(x, "date_onset")
+}
+
+#' @export
+#' @rdname select.linelist
+select_date_reporting <- function(x) {
+  select(x, "date_reporting")
+}
+
+#' @export
+#' @rdname select.linelist
+select_date_admission <- function(x) {
+  select(x, "date_admission")
+}
+
+#' @export
+#' @rdname select.linelist
+select_date_discharge <- function(x) {
+  select(x, "date_discharge")
+}
+
+#' @export
+#' @rdname select.linelist
+select_date_outcome <- function(x) {
+  select(x, "date_outcome")
+}
+
+#' @export
+#' @rdname select.linelist
+select_date_death <- function(x) {
+  select(x, "date_death")
+}
+
+#' @export
+#' @rdname select.linelist
+select_gender <- function(x) {
+  select(x, "gender")
+}
+
+#' @export
+#' @rdname select.linelist
+select_age <- function(x) {
+  select(x, "age")
+}
+
+#' @export
+#' @rdname select.linelist
+select_location <- function(x) {
+  select(x, "location")
+}
+
+#' @export
+#' @rdname select.linelist
+select_occupation <- function(x) {
+  select(x, "occupation")
+}
+
+#' @export
+#' @rdname select.linelist
+select_hcw <- function(x) {
+  select(x, "hcw")
+}
+
+#' @export
+#' @rdname select.linelist
+select_outcome <- function(x) {
+  select(x, "outcome")
 }
