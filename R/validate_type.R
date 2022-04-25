9c3921ef47e4d738909ed6b76d821f5ff9079f84
Thibaut Jombart <thibautjombart@gmail.com>
2022-04-25 19:03:26 +0200

    placeholder for new function

---
+++ R/validate_type.R
@@ -0,0 +1,19 @@
+#' Check a tagged variable has the right type
+#'
+#' This function checks that a specific tagged variable has the expected type of
+#' data. Default expected types are defined by [`tags_types`](tags_types).
+#'
+#' @export
+#'
+#' @author Thibaut Jombart [thibaut@data.org](thibaut@data.org)
+#'
+#' @return A named `list`.
+#'
+#' @seealso [`tags_types`](tags_types) to check or change acceptable data
+#'   classes for specific tags
+#' 
+#' @examples
+#' 
+
+validate_type <- function() {
+}
