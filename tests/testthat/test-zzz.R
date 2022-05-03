2bb032c3ba51f97585c36d0d2256c41a334ce823
Thibaut Jombart <thibautjombart@gmail.com>
2022-05-03 17:59:27 +0200

    adding test for zzz

---
+++ tests/testthat/test-zzz.R
@@ -0,0 +1,11 @@
+test_that("tests for zzz", {
+  # We need to use callr to avoid conflicts with other tests
+  res <- callr::r(
+    function() {
+      library(linelist)
+      get_lost_tags_action()
+    })
+  expect_identical(res, "warning")
+  
+})
+
