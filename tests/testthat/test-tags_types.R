ddd9baa12345d9f80dc3de166067b7cdf52cb88a
Thibaut Jombart <thibautjombart@gmail.com>
2022-05-03 17:36:40 +0200

    adding tests

--- tests/testthat/test-tags_types.R
+++ tests/testthat/test-tags_types.R
@@ -7,7 +7,7 @@ test_that("tests for tags_types", {
   # Check functionality
   x <- tags_types()
   expect_identical(tags_names(), names(x))
-  expect_is(x, "list")
+  expect_type(x, "list")
   expect_true(all(sapply(x, is.character)))
 
   x <- tags_types(date_outcome = "Date")
