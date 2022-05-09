dbb5bef50f5112756b0377ec457c4ff54f680749
Thibaut Jombart <thibautjombart@gmail.com>
2022-05-09 11:49:22 +0200

    adding missing tests

--- tests/testthat/test-lost_tags_action.R
+++ tests/testthat/test-lost_tags_action.R
@@ -1,5 +1,14 @@
 test_that("tests for lost_tags_action", {
 
+  msg <- "Lost tags will now issue a warning."
+  expect_message(lost_tags_action(), msg)
+
+  msg <- "Lost tags will now issue an error."
+  expect_message(lost_tags_action("error"), msg)
+
+  msg <- "Lost tags will now be ignored."
+  expect_message(lost_tags_action("none"), msg)
+
   lost_tags_action("error", quiet = TRUE)
   expect_identical(get_lost_tags_action(), "error")
 
@@ -12,5 +21,5 @@ test_that("tests for lost_tags_action", {
 
   lost_tags_action("warning", quiet = TRUE)
   expect_identical(get_lost_tags_action(), "warning")
-
+  
 })
