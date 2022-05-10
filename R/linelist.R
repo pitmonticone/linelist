303a92fe9e53e25bff2a9b90afc1fa67bd54a6a4
Thibaut Jombart <thibautjombart@gmail.com>
2022-05-10 15:11:42 +0200

    fixing broken links

--- R/linelist.R
+++ R/linelist.R
@@ -14,26 +14,26 @@
 #'
 #' @aliases linelist
 #' 
-#' @author Thibaut Jombart [thibaut@data.org](thibaut@data.org)
+#' @author Thibaut Jombart \email{thibaut@@data.org}
 #'
 #' @section Main functions:
 #'
-#' * [`make_linelist`](make_linelist): to create `linelist` objects from a
+#' * [make_linelist()]: to create `linelist` objects from a
 #' `data.frame` or a `tibble`, with the possibility to tag key epi variables
 #'
-#' * [`set_tags`](set_tags): to change or add tagged variables in a `linelist`
+#' * [set_tags()]: to change or add tagged variables in a `linelist`
 #' 
-#' * [`tags`](): to get the list of tags of a `linelist`
+#' * [tags()]: to get the list of tags of a `linelist`
 #' 
-#' * [`tags_df`](tags_df): to get a `data.frame` of all tagged variables
+#' * [tags_df()]: to get a `data.frame` of all tagged variables
 #' 
-#' * [`select_tags`](select_tags): like `dplyr::select`, but for tagged variables
+#' * [select_tags()]: like [dplyr::select()], but for tagged variables
 #' 
-#' * [`lost_tags_action`](lost_tags_action): to change the behaviour of actions
+#' * [lost_tags_action()]: to change the behaviour of actions
 #' where tagged variables are lost (e.g. removing columns storing tagged
 #' variables) to issue warnings, errors, or do nothing
 #' 
-#' * [`get_lost_tags_action`](lost_tags_action): to check the current
+#' * [get_lost_tags_action()]: to check the current
 #' behaviour of actions where tagged variables are lost
 #'
 #' @section Dedicated methods:
@@ -43,10 +43,9 @@
 #'   alter or lose tagged variables (and may thus break downstream data
 #'   pipelines).
 #' 
-#' * [`names() <-`](names<-.linelist) and [`rename`](rename.linelist): will
-#' rename tags as needed
+#' * `names() <-` and `rename` (see [rename.linelist()]): will rename tags as needed
 #' 
-#' * [`x[...] <-`](sub_linelist) and [`x[[...]] <-`](sub_linelist): will adopt
+#' * `x[...] <-` and `x[[...]] <-` (see [sub_linelist]): will adopt
 #' the desire behaviour when tagged variables are lost
 #'
 #' * `print()`: prints info about the `linelist` in addition to the `data.frame`
